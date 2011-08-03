0e4c56f022b007a7e863262d4e6fb5230b1d0eba
yuchting <yuchting@gmail.com>
2011-08-03 11:16:47 +0000

    modify/addInWork

--- yuchsign/src/com/yuchting/yuchberry/yuchsign/server/yuchbber.java
+++ yuchsign/src/com/yuchting/yuchberry/yuchsign/server/yuchbber.java
@@ -38,7 +38,7 @@ public final class yuchbber {
 	private String m_password = "";
 	
 	@Persistent
-	private long m_usingHours = 120;
+	private long m_usingHours = 360;
 	
 	@Persistent
 	private long m_createTime = 0;
