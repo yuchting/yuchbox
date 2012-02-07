4887ac840023c2d40bd1d99d4586435d935f6ea7
yuchting <yuchting@gmail.com>
2012-02-07 10:11:32 +0000

    modify/addInWork

--- server/src/com/yuchting/yuchberry/server/HelloWorld.java
+++ server/src/com/yuchting/yuchberry/server/HelloWorld.java
@@ -28,55 +28,28 @@
 package com.yuchting.yuchberry.server;
 
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.KeyStore;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
-import java.util.Vector;
 import java.util.concurrent.Callable;
 
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocketFactory;
-import javax.swing.JButton;
-import javax.swing.JEditorPane;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
-import javax.swing.JToolBar;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
-import javax.swing.filechooser.FileFilter;
 
 import weibo4j.http.OAuth;
 import weibo4j.http.OAuthToken;
@@ -153,17 +126,6 @@ public class HelloWorld {
 	
 	public static void main(String arg[])throws Exception{
 	
-		char a = 0x33;
-		char b = 0x36;
-		StringBuffer t_bf = new StringBuffer();
-		
-		char c = 0x39;
-		
-		t_bf.append(a).append(b);
-		
-		cryptPassword t_crypt = new cryptPassword(t_bf.append(c).toString());
-		System.out.println(t_crypt.encrypt("CDUH1L5EuszWjiU1y8firw"));
-		System.out.println(t_crypt.encrypt("1CGdAVmxaezBQNzgKJvRkXM8WL5tQ7itAosMwp9QlMg"));
 	}
 	
 	public final static String	fsm_vectStringSpliter = "<>";
