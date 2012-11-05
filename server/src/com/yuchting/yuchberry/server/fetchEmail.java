b8008cca467d38e0115caf6f04afcc25680f0fbc
yuchting <yuchting@gmail.com>
2012-11-05 12:45:25 +0000

    删除 标题信息的明文输出

--- server/src/com/yuchting/yuchberry/server/fetchEmail.java
+++ server/src/com/yuchting/yuchberry/server/fetchEmail.java
@@ -1585,7 +1585,7 @@ public class fetchEmail extends fetchAccount{
 				t_mail.m_sendConfirmNum++;
 				
 				m_mainMgr.m_logger.LogOut(GetAccountName() + " send mail<" + t_mail.GetMailIndex() + " : "
-						 				+ "simpleHash<"+ t_mail.GetSimpleHashCode() + " " + t_mail.GetSubject() + "+" + t_mail.GetSendDate().getTime() + ">,wait confirm...");
+						 				+ "simpleHash<"+ t_mail.GetSimpleHashCode() + ">,wait confirm...");
 				
 				// statistics
 				//
