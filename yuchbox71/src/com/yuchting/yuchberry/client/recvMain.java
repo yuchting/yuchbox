d6c4b04f77ae1cf0190153bf19f6a408256f97d2
yuchting <yuchting@gmail.com>
2013-01-31 09:47:58 +0000

    修改 合并所有主版本从 1971 依赖的修改 修改 提交BBM 版本的： 保护性保存文件时候的bug（不然导致保护性保存失效）

--- yuchbox71/src/com/yuchting/yuchberry/client/recvMain.java
+++ yuchbox71/src/com/yuchting/yuchberry/client/recvMain.java
@@ -568,6 +568,12 @@ public class recvMain extends UiApplication implements yblocalResource,LocationL
 		}
 	}
 	
+	private void endFlurry(){
+		if(m_flurryKey != null){
+			FlurryAgent.onDestroyApp();
+		}
+	}
+	
 	/**
 	 * prepare the APN value
 	 */
@@ -761,6 +767,8 @@ public class recvMain extends UiApplication implements yblocalResource,LocationL
 			public void run() {
 				synchronized(getEventLock()) {
 					Dialog.alert(_msg);
+					
+					endFlurry();	
 					System.exit(0);
 				}
 			}
@@ -1056,7 +1064,7 @@ public class recvMain extends UiApplication implements yblocalResource,LocationL
 			
 			int tSlashIdx = _pathFilename.lastIndexOf('/');
 			
-			String tParentPath	= _pathFilename.substring(0,tSlashIdx);
+			String tParentPath	= _pathFilename.substring(0,tSlashIdx + 1);
 			String tOrgFilename = _pathFilename.substring(tSlashIdx + 1);
 			
 			String tBackFilename		= "~" + tOrgFilename;
@@ -1115,7 +1123,7 @@ public class recvMain extends UiApplication implements yblocalResource,LocationL
 		try{
 			int tSlashIdx = _pathFilename.lastIndexOf('/');
 			
-			String tParentPath			= _pathFilename.substring(0,tSlashIdx);
+			String tParentPath			= _pathFilename.substring(0,tSlashIdx + 1);
 			String tOrgFilename 		= _pathFilename.substring(tSlashIdx + 1);
 			
 			String tBackFilename		= "~" + tOrgFilename;
@@ -1662,6 +1670,7 @@ public class recvMain extends UiApplication implements yblocalResource,LocationL
 		
 		// store the weibo item list
 		if(ReadWriteWeiboFile(false)){
+			endFlurry();
 			System.exit(0);
 		}		
 		
@@ -2913,6 +2922,7 @@ public class recvMain extends UiApplication implements yblocalResource,LocationL
 				public void run() {
 					readWriteWeiboFile_impl(_read);
 					
+					endFlurry();
 					System.exit(0);
 				}
 			},100,false);
